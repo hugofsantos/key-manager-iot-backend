0ffa52b2b7ee2491c889635bd91ca1e2b5d0bcbf
Hugo Santos <hf.santos2332@gmail.com>
2023-06-29 20:05:51 -0300

    Corrige problema de cors

--- src/app.js
+++ src/app.js
@@ -11,6 +11,13 @@ connectToDatabase(process.env.MONGO_HOST, process.env.MONGO_PORT, process.env.MO
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json());
 app.use(routes);
 
